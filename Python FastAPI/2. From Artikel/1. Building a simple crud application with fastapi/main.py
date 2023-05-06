88a0b54e0d10800ac0bc845cadf7022cd243c93d
Taufik <informatikataufik@gmail.com>
2023-05-06 15:26:18 +0700

    python

---
+++ Python FastAPI/2. From Artikel/1. Building a simple crud application with fastapi/main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI, status
+from database import Base, engine, ToDo
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+# Create ToDoRequest Base Model
+class ToDoRequest(BaseModel):
+    task: str
+
+# Create the database
+Base.metadata.create_all(engine)
+
+# Initialize app
+app = FastAPI()
+
+# <irrelevant code here..>
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+def create_todo(todo: ToDoRequest):
+
+    # create a new database session
+    session = Session(bind=engine, expire_on_commit=False)
+
+    # create an instance of the ToDo database model
+    tododb = ToDo(task = todo.task)
+
+    # add it to the session and commit it
+    session.add(tododb)
+    session.commit()
+
+    # grab the id given to the object from the database
+    id = tododb.id
+
+    # close the session
+    session.close()
+
+    # return the id
+    return f"created todo item with id {id}"
+
+@app.get("/todo/{id}")
+def read_todo(id: int):
+    return "read todo item with id {id}"
+
+@app.put("/todo/{id}")
+def update_todo(id: int):
+    return "update todo item with id {id}"
+
+@app.delete("/todo/{id}")
+def delete_todo(id: int):
+    return "delete todo item with id {id}"
+
+@app.get("/todo")
+def read_todo_list():
+    return "read todo list"
